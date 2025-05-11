993d16a941b9216804b7902640662d090b011031
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-11 23:38:48 +0700

    update

--- src/components/RecentlyHeard/index.jsx
+++ src/components/RecentlyHeard/index.jsx
@@ -20,6 +20,16 @@ export default function RecentlyHeard() {
         "https://photo-playlist-zmp3.zmdcdn.me/s1/user-playlist?src=HavwqN7EvKCI1oYSFOdqNqjITOmzYRWE1b8cY6YFiGbK0YgCFzZbMWLMAOzkXRy81rmft3tejbXGC2pQFEYnN4jfRuimhkDN02icqtlwgW1DPs72PwEfNLXdOOjWxVqANpPYYpQyg0rPPc3OCBhwMGjaDOemlVbSM6O8tJMxbLjUOmpRDwR86mblIeayxOfILYGUm6oWnXL7E4V3CR765GfjMS5ajCz236rGop3WnqnOFGZ9Ck-LJ5Wo0ifWl9Kb26CCfcAva20CPH3w9UcSPbvkMxXijzSo5pyMz2kcosiJObRqAxh0VKjeMVrmuizyIpnKg7Urco4MQ5JpTx6V9XXZLQ4UkSyaT6jLenguo3qf8m6lVG&size=thumb/240_240",
       title: "Album Karic",
     },
+    {
+      image:
+        "https://photo-resize-zmp3.zmdcdn.me/w600_r1x1_jpeg/cover/2/9/5/d/295d1acf510893079a8d92cc0bc92120.jpg",
+      title: "Playlist Này Chill Phết",
+    },
+    {
+      image:
+        "https://photo-playlist-zmp3.zmdcdn.me/s1/user-playlist?src=HavwqN7EvKCI1oYSFOdqNqjITOmzYRWE1b8cY6YFiGbK0YgCFzZbMWLMAOzkXRy81rmft3tejbXGC2pQFEYnN4jfRuimhkDN02icqtlwgW1DPs72PwEfNLXdOOjWxVqANpPYYpQyg0rPPc3OCBhwMGjaDOemlVbSM6O8tJMxbLjUOmpRDwR86mblIeayxOfILYGUm6oWnXL7E4V3CR765GfjMS5ajCz236rGop3WnqnOFGZ9Ck-LJ5Wo0ifWl9Kb26CCfcAva20CPH3w9UcSPbvkMxXijzSo5pyMz2kcosiJObRqAxh0VKjeMVrmuizyIpnKg7Urco4MQ5JpTx6V9XXZLQ4UkSyaT6jLenguo3qf8m6lVG&size=thumb/240_240",
+      title: "Album Karic",
+    },
   ];
   return (
     <>
